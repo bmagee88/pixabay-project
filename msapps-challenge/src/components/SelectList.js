f473ccfc687a745b8ea9bcfd698496d8502604d7
Brian <53064162+bmagee88@users.noreply.github.com>
2024-02-21 12:24:17 +0200

    mod selectList, added redux

--- msapps-challenge/src/components/SelectList.js
+++ msapps-challenge/src/components/SelectList.js
@@ -1,16 +1,30 @@
 import { Button, Typography } from "@mui/material";
 import React from "react";
+import { useDispatch } from "react-redux";
+import { setCategory } from "../state/category/categorySlice";
+import { toggleCategoryModalClose } from "../state/modal/categoryModalSlice";
 
-const SelectList = ({ list, selectedItem, setSelectedItem }) => {
-
-
+const SelectList = ({ list }) => {
+  const dispatch = useDispatch();
   return (
     <div>
       {list.map((item) => {
-        const fontWeight = selectedItem === item.name ? "800" : "500" ;
+        // const fontWeight = selectedItem === item.name ? "800" : "500" ;
+        const fontWeight = 500;
         return (
-          <Button key={item.id} sx={{ "&:hover": { background: "#f7f7f7" } }} onClick={()=>setSelectedItem(item.name)}>
-            <Typography key={item.id} id={item.id} sx={{ mt: 2, fontWeight: fontWeight }}>
+          <Button
+            key={item.id}
+            sx={{ "&:hover": { background: "#f7f7f7" } }}
+            onClick={() => {
+              dispatch(setCategory(item.name));
+              dispatch(toggleCategoryModalClose());
+            }}
+          >
+            <Typography
+              key={item.id}
+              id={item.id}
+              sx={{ mt: 2, fontWeight: fontWeight }}
+            >
               {item.name}
             </Typography>
           </Button>
