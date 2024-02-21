72168522ad1fb8e72ea37376de84adfe45f0704d
Brian <53064162+bmagee88@users.noreply.github.com>
2024-02-21 12:22:27 +0200

    mod store, add registration for category, imageModal and categoryModal

--- msapps-challenge/src/state/store.js
+++ msapps-challenge/src/state/store.js
@@ -1,10 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import imageReducer from "./image/imageSlice";
 import pageReducer from "./page/pageSlice";
+import categoryReducer from './category/categorySlice'
+import categoryModalReducer from './modal/categoryModalSlice'
 import { combineReducers } from "redux";
 
 const reducers = combineReducers({
   images: imageReducer,
   page: pageReducer,
+  category: categoryReducer,
+  imageModal: imageReducer,
+  categoryModal: categoryModalReducer
 });
 export const store = configureStore({ reducer: reducers });
