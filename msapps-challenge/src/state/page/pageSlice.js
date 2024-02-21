9ef82953202c8d529efdaed5a286af85290c5a3a
Brian <53064162+bmagee88@users.noreply.github.com>
2024-02-21 12:20:31 +0200

    mod pageSlice, add reset reducer

--- msapps-challenge/src/state/page/pageSlice.js
+++ msapps-challenge/src/state/page/pageSlice.js
@@ -11,10 +11,13 @@ const pageSlice = createSlice({
         },
         decrement(state) {
             state.value = state.value <= 1 ? 1 : state.value - 1;
+        },
+        reset(state) {
+            state.value = 1;
         }
     }
 })
 
-export const {increment, decrement} = pageSlice.actions;
+export const {increment, decrement, reset} = pageSlice.actions;
 
 export default pageSlice.reducer;
