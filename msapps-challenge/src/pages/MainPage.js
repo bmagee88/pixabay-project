f00769adf69a41df3e85022d345cf529668a094d
Brian <53064162+bmagee88@users.noreply.github.com>
2024-02-21 12:23:45 +0200

    mod mainpage, extract api calls into api.js, remove useState replaced with redux, rm testing code, rm prop drilling

--- msapps-challenge/src/pages/MainPage.js
+++ msapps-challenge/src/pages/MainPage.js
@@ -6,19 +6,8 @@ import NextButton from "../components/NextButton";
 import ImageViewer from "../components/ImageViewer";
 import CategoryModal from "../components/CategoryModal";
 import { useDispatch, useSelector } from "react-redux";
-import { setImages } from '../state/image/imageSlice';
-
-const mock_category_list = [
-  { id: 0, name: "nature" },
-  { id: 1, name: "sports" },
-  { id: 2, name: "animals" },
-  { id: 3, name: "architecture" },
-  { id: 4, name: "stocks" },
-  { id: 5, name: "coffee" },
-  { id: 6, name: "programming" },
-  { id: 7, name: "pythons" },
-  { id: 8, name: "rest" },
-];
+import { setImages } from "../state/image/imageSlice";
+import { fetchImages, manageSort } from "../api/api";
 
 
 
@@ -29,63 +18,35 @@ const style = {
 };
 
 const MainPage = () => {
-  const localhost = `http://localhost:8000`;
-  const PER_PAGE = 9;
-
-  const [isCategoryModalOpen, setIsCategoryModalOpen] = useState(false);
-  const [category, setCategory] = useState(mock_category_list[0].name);
+  const category = useSelector((state) => state.category.value);
   const page = useSelector((state) => state.page.value);
-  const images = useSelector((state) => state.images.value);
+  // const images = useSelector((state) => state.images.value);
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  // useEffect(() => {
-  //   const fetchImages = async () => {
-  //     let url = `${localhost}/category?category=${category}&page=${page}&per_page=${PER_PAGE}`;
-  //     const response = await fetch(url);
-  //     const data = await response.json();
-  //     dispatch(setImages(data.hits))
-  //   };
-  //   fetchImages();
-  // }, [localhost, category, page, dispatch]);
+  useEffect(() => {
+    const getImages = async () => {
+      const data = await fetchImages(category, page);
+      dispatch(setImages(data));
+    };
+    getImages();
+  }, [category, page, dispatch]);
 
-  const handleCategoryModalClose = () => {
-    setIsCategoryModalOpen(false);
-  };
 
   const [sortKey, setSortKey] = useState("id");
   const [orderAsc, setOrderAsc] = useState(false);
 
-  const handleSort = () => {
-    const sortImagesFetch = async () => {
-      let url = `${localhost}/sort`;
-      const dto = {
-        list_to_sort: images,
-        sort_by: sortKey,
-        order_asc: orderAsc,
-      };
-      const response = await fetch(url, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(dto),
-      });
-      const data = await response.json();
-      dispatch(setImages(data))
-    };
-    if (sortKey === undefined || orderAsc === undefined) return;
-    sortImagesFetch();
+  const handleSort = async () => {
+    const sortedData = await manageSort(sortKey, orderAsc, category, page);
+    dispatch(setImages(sortedData));
   };
 
-  const buttonSelectStyle = (target, value) =>  {return { background: target === value ? "#1C4966" : "" }};
+  const buttonSelectStyle = (target, value) => {
+    return { background: target === value ? "#1C4966" : "" };
+  };
 
   return (
     <Box id="main-page" sx={style}>
-      current page:{page}
-      <br></br>
-      sort by value:{sortKey}
-      <br></br>
-      order Asc:{orderAsc !== undefined ? orderAsc.toString() : ""}
-      <br></br>
       <Box sx={{ display: "flex", gap: "2rem", paddingY: "2rem" }}>
         sort by:
         <Button
@@ -136,15 +97,11 @@ const MainPage = () => {
       </Box>
       <Box id="nav" sx={{ display: "flex", justifyContent: "space-evenly" }}>
         <PrevButton />
-        <CategoryButton setIsOpen={setIsCategoryModalOpen} />
+        <CategoryButton />
         <NextButton />
       </Box>
       <ImageViewer />
       <CategoryModal
-        isOpen={isCategoryModalOpen}
-        handleClose={handleCategoryModalClose}
-        categoryList={mock_category_list}
-        setCategory={setCategory}
       />
     </Box>
   );
